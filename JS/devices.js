6e10f947e22c082c7317b5c54306ceab52293b41
Raven117k <luciferplayer31@gmail.com>
2026-01-10 11:28:18 +0500

    90%

--- JS/devices.js
+++ JS/devices.js
@@ -1,40 +1,40 @@
 const DEVICES = {
   iphone8: {
     name: "iPhone 8 / SE2 / SE3",
-    viewport: { w: 330, h: 565 },
-    frame: { w: 363, h: 750 },
+    viewport: { w: 375, h: 667 },
+    frame: { w: 435, h: 890 },
     statusBarHeight: 20,
     type: "ios",
     image: "./frames/iphone-se.png"
   },
   iphone14: {
     name: "iPhone 14",
-    viewport: { w: 350, h: 805 },
-    frame: { w: 390, h: 844 },
+    viewport: { w: 390, h: 844 },
+    frame: { w: 435, h: 886 },
     statusBarHeight: 32,
     type: "ios",
     image: "./frames/iphone14.png"
   },
   iphone14Pro: {
     name: "iPhone 14 Pro",
-    viewport: { w: 393, h: 852 },
-    frame: { w: 393, h: 852 },
-    statusBarHeight: 59,
+    viewport: { w: 397, h: 852 },
+    frame: { w: 445, h: 897 },
+    statusBarHeight: 50,
     type: "ios",
-    image: "https://uploads-ssl.webflow.com/61f50d4b7b6ec346656e2b57/637e9d9c18bde3563e5f0fb6_iPhone%2014.svg"
+    image: "./frames/iphone14pro.png"
   },
   pixel7: {
     name: "Pixel 7",
-    viewport: { w: 420, h: 915 },
-    frame: { w: 450, h: 960 },
+    viewport: { w: 412, h: 915 },
+    frame: { w: 435, h: 950 },
     statusBarHeight: 32,
     type: "android",
     image: "./frames/pixel7.png"
   },
   samsungS22: {
     name: "Samsung S22",
-    viewport: { w: 335, h: 720 },
-    frame: { w: 363, h: 750 },
+    viewport: { w: 360, h: 780 },
+    frame: { w: 383, h: 815 },
     statusBarHeight: 24,
     type: "android",
     image: "./frames/s22.png"
