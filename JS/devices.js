a52bbbd84be42530ad3c0a709c5d4bda40781aae
Raven117k <luciferplayer31@gmail.com>
2026-01-11 21:06:42 +0500

    new devices added

--- JS/devices.js
+++ JS/devices.js
@@ -31,6 +31,78 @@ const DEVICES = {
     type: "ios",
     image: "./frames/iphoneX.png"
   },
+  iphone11: {
+    name: "iPhone 11",
+    viewport: { w: 414, h: 896 },
+    frame: { w: 465, h: 970 },
+    statusBarHeight: 32,
+    type: "ios",
+    image: "./frames/iphone11.png"
+  },
+  iphone11pro: {
+    name: "iPhone 11 Pro",
+    viewport: { w: 375, h: 812 },
+    frame: { w: 430, h: 870 },
+    statusBarHeight: 32,
+    type: "ios",
+    image: "./frames/iphone11pro.png"
+  },
+  iphone11promax: {
+    name: "iPhone 11 Pro Max",
+    viewport: { w: 414, h: 896 },
+    frame: { w: 465, h: 950 },
+    statusBarHeight: 32,
+    type: "ios",
+    image: "./frames/iphone11pm.png"
+  },
+   iphone12: {
+    name: "iPhone 12",
+    viewport: { w: 390, h: 844 },
+    frame: { w: 435, h: 886 },
+    statusBarHeight: 32,
+    type: "ios",
+    image: "./frames/iphone12.png"
+  },
+  iphone12pro: {
+    name: "iPhone 12 Pro",
+    viewport: { w: 390, h: 844 },
+    frame: { w: 435, h: 886 },
+    statusBarHeight: 32,
+    type: "ios",
+    image: "./frames/iphone12pro.png"
+  },
+  iphone12promax: {
+    name: "iPhone 12 Pro Max",
+    viewport: { w: 428, h: 926 },
+    frame: { w: 470, h: 973 },
+    statusBarHeight: 32,
+    type: "ios",
+    image: "./frames/iphone12pm.png"
+  },
+  iphone13: {
+    name: "iPhone 13",
+    viewport: { w: 390, h: 844 },
+    frame: { w: 435, h: 886 },
+    statusBarHeight: 32,
+    type: "ios",
+    image: "./frames/iphone13.png"
+  },
+  iphone13pro: {
+    name: "iPhone 13 Pro",
+    viewport: { w: 390, h: 844 },
+    frame: { w: 435, h: 886 },
+    statusBarHeight: 32,
+    type: "ios",
+    image: "./frames/iphone13pro.png"
+  },
+  iphone13promax: {
+    name: "iPhone 13 Pro Max",
+    viewport: { w: 428, h: 926 },
+    frame: { w: 470, h: 973 },
+    statusBarHeight: 32,
+    type: "ios",
+    image: "./frames/iphone13pm.png"
+  },
   iphone14: {
     name: "iPhone 14",
     viewport: { w: 390, h: 844 },
@@ -47,6 +119,38 @@ const DEVICES = {
     type: "ios",
     image: "./frames/iphone14pro.png"
   },
+  iphone14ProMax: {
+    name: "iPhone 14 Pro Max",
+    viewport: { w: 430, h: 932 },
+    frame: { w: 478, h: 981 },
+    statusBarHeight: 50,
+    type: "ios",
+    image: "./frames/iphone14pm.png"
+  },
+  iphone15: {
+    name: "iPhone 15",
+    viewport: { w: 393, h: 852 },
+    frame: { w: 435, h: 886 },
+    statusBarHeight: 39,
+    type: "ios",
+    image: "./frames/iphone15.png"
+  },
+  iphone15Pro: {
+    name: "iPhone 15 Pro",
+    viewport: { w: 395, h: 852 },
+    frame: { w: 435, h: 890 },
+    statusBarHeight: 39,
+    type: "ios",
+    image: "./frames/iphone15pro.png"
+  },
+  iphone15ProMax: {
+    name: "iPhone 15 Pro Max",
+    viewport: { w: 430, h: 932 },
+    frame: { w: 470, h: 968 },
+    statusBarHeight: 50,
+    type: "ios",
+    image: "./frames/iphone15pm.png"
+  },
   pixel7: {
     name: "Pixel 7",
     viewport: { w: 412, h: 915 },
@@ -62,5 +166,8 @@ const DEVICES = {
     statusBarHeight: 24,
     type: "android",
     image: "./frames/s22.png"
+  },
+  NewUpdate: {
+    name: "More Devices Comming Soon!",
   },
 };
